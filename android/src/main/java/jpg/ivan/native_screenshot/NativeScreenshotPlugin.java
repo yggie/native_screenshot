590848a7f36e4bde0fae508a17d1f0810360804a
Bryan Yap <bryan@amata.world>
2022-07-15 21:00:39 +0100

    update to use the new MediaStore API

--- android/src/main/java/jpg/ivan/native_screenshot/NativeScreenshotPlugin.java
+++ android/src/main/java/jpg/ivan/native_screenshot/NativeScreenshotPlugin.java
@@ -2,15 +2,22 @@ package jpg.ivan.native_screenshot;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -19,6 +26,9 @@ import androidx.annotation.NonNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
 import java.util.Date;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -220,17 +230,42 @@ public class NativeScreenshotPlugin implements MethodCallHandler, FlutterPlugin,
 	} // getScreenshotPath()
 
 	private String writeBitmap(Bitmap bitmap) {
+		final String relPath = getApplicationName() + File.separator + getScreenshotName();
+		final ContentValues values = new ContentValues();
+		values.put(MediaStore.MediaColumns.DISPLAY_NAME, relPath);
+		values.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+		values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+
+		final ContentResolver resolver = this.context.getContentResolver();
+		Uri uri = null;
+
 		try {
-			String path = getScreenshotPath();
-			File imageFile = new File(path);
-			FileOutputStream oStream = new FileOutputStream(imageFile);
+			uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
-			bitmap.compress(Bitmap.CompressFormat.PNG, 100, oStream);
-			oStream.flush();
-			oStream.close();
+			if (uri == null)
+				throw new IOException("Failed to create new MediaStore record");
 
-			return path;
+			final OutputStream stream = resolver.openOutputStream(uri);
+
+			if (stream == null)
+				throw new IOException("Failed to open output stream to MediaStore");
+
+			if (!bitmap.compress(Bitmap.CompressFormat.PNG, 95, stream))
+				throw new IOException("Failed to save bitmap to MediaStore");
+
+			// String path = getScreenshotPath();
+			// File imageFile = new File(path);
+			// FileOutputStream oStream = new FileOutputStream(imageFile);
+
+			// bitmap.compress(Bitmap.CompressFormat.PNG, 100, oStream);
+			// oStream.flush();
+			// oStream.close();
+
+			return getPath(this.context, uri);
 		} catch (Exception ex) {
+			if (uri != null)
+				resolver.delete(uri, null, null);
+
 			Log.println(Log.INFO, TAG, "Error writing bitmap: " + ex.getMessage());
 		}
 
@@ -383,4 +418,175 @@ public class NativeScreenshotPlugin implements MethodCallHandler, FlutterPlugin,
 
 		return false;
 	} // permissionToWrite()
+
+	// CODE BELOW IS AN EXCERP FROM
+	// https://github.com/iPaulPro/aFileChooser/blob/master/aFileChooser/src/com/ipaulpro/afilechooser/utils/FileUtils.java
+
+	/**
+	 * Get a file path from a Uri. This will get the the path for Storage Access
+	 * Framework Documents, as well as the _data field for the MediaStore and
+	 * other file-based ContentProviders.<br>
+	 * <br>
+	 * Callers should check whether the path is local before assuming it
+	 * represents a local file.
+	 * 
+	 * @param context The context.
+	 * @param uri     The Uri to query.
+	 * @see #isLocal(String)
+	 * @see #getFile(Context, Uri)
+	 * @author paulburke
+	 */
+	public static String getPath(final Context context, final Uri uri) {
+
+		// if (DEBUG)
+		// Log.d(TAG + " File -",
+		// "Authority: " + uri.getAuthority() +
+		// ", Fragment: " + uri.getFragment() +
+		// ", Port: " + uri.getPort() +
+		// ", Query: " + uri.getQuery() +
+		// ", Scheme: " + uri.getScheme() +
+		// ", Host: " + uri.getHost() +
+		// ", Segments: " + uri.getPathSegments().toString());
+
+		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+		// DocumentProvider
+		if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+			// // LocalStorageProvider
+			// if (isLocalStorageDocument(uri)) {
+			// // The path is the id
+			// return DocumentsContract.getDocumentId(uri);
+			// }
+			// ExternalStorageProvider
+			if (isExternalStorageDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				if ("primary".equalsIgnoreCase(type)) {
+					return Environment.getExternalStorageDirectory() + "/" + split[1];
+				}
+
+				// TODO handle non-primary volumes
+			}
+			// DownloadsProvider
+			else if (isDownloadsDocument(uri)) {
+
+				final String id = DocumentsContract.getDocumentId(uri);
+				final Uri contentUri = ContentUris.withAppendedId(
+						Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+				return getDataColumn(context, contentUri, null, null);
+			}
+			// MediaProvider
+			else if (isMediaDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				Uri contentUri = null;
+				if ("image".equals(type)) {
+					contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+				} else if ("video".equals(type)) {
+					contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+				} else if ("audio".equals(type)) {
+					contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+				}
+
+				final String selection = "_id=?";
+				final String[] selectionArgs = new String[] {
+						split[1]
+				};
+
+				return getDataColumn(context, contentUri, selection, selectionArgs);
+			}
+		}
+		// MediaStore (and general)
+		else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+			// Return the remote address
+			if (isGooglePhotosUri(uri))
+				return uri.getLastPathSegment();
+
+			return getDataColumn(context, uri, null, null);
+		}
+		// File
+		else if ("file".equalsIgnoreCase(uri.getScheme())) {
+			return uri.getPath();
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 * @author paulburke
+	 */
+	public static boolean isExternalStorageDocument(Uri uri) {
+		return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 * @author paulburke
+	 */
+	public static boolean isDownloadsDocument(Uri uri) {
+		return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 * @author paulburke
+	 */
+	public static boolean isMediaDocument(Uri uri) {
+		return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is Google Photos.
+	 */
+	public static boolean isGooglePhotosUri(Uri uri) {
+		return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+	}
+
+	/**
+	 * Get the value of the data column for this Uri. This is useful for
+	 * MediaStore Uris, and other file-based ContentProviders.
+	 *
+	 * @param context       The context.
+	 * @param uri           The Uri to query.
+	 * @param selection     (Optional) Filter used in the query.
+	 * @param selectionArgs (Optional) Selection arguments used in the query.
+	 * @return The value of the _data column, which is typically a file path.
+	 * @author paulburke
+	 */
+	public static String getDataColumn(Context context, Uri uri, String selection,
+			String[] selectionArgs) {
+
+		Cursor cursor = null;
+		final String column = "_data";
+		final String[] projection = {
+				column
+		};
+
+		try {
+			cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+					null);
+			if (cursor != null && cursor.moveToFirst()) {
+				// if (DEBUG)
+				// DatabaseUtils.dumpCursor(cursor);
+
+				final int column_index = cursor.getColumnIndexOrThrow(column);
+				return cursor.getString(column_index);
+			}
+		} finally {
+			if (cursor != null)
+				cursor.close();
+		}
+		return null;
+	}
 } // NativeScreenshotPlugin
